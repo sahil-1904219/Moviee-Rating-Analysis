97861f2d3ffb7b121430eea1d643e5c1aec995cc
Sahil Anjum <67681012+sahil-1904219@users.noreply.github.com>
2023-02-15 19:29:55 +0530

    Add files via upload

---
+++ attched css files/script.js
@@ -0,0 +1,21 @@
+function openForm1() {
+    document.getElementById("myForm1").style.display = "block";
+  }
+  
+  function closeForm1() {
+    document.getElementById("myForm1").style.display = "none";
+  }
+function openForm2() {
+    document.getElementById("myForm2").style.display = "block";
+  }
+  
+  function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+  }
+function openForm3() {
+    document.getElementById("myForm3").style.display = "block";
+  }
+  
+  function closeForm3() {
+    document.getElementById("myForm3").style.display = "none";
+  }
---
+++ script.js
@@ -0,0 +1,7 @@
+function openForm2() {
+    document.getElementById("myForm2").style.display = "block";
+  }
+  
+  function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+  }
